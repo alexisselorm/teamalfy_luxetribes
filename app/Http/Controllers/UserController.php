cf4f4836820c4da313f9904ae77d840f68a884c4
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2022-11-10 17:02:19 +0000

    Added route links for users

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,48 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function create()
+    {
+        return view('users.register');
+    }
+
+    public function login(){
+        return view('users.login');
+    }
+
+    public function authenticate(){
+        $formfields= request()->validate([
+            'email'=> ['required', 'email'],
+            'password' => ['required']
+        ]);
+        if(auth()->attempt($formfields)){
+            request()->session()->regenerate();
+
+            return redirect('/')->with('message', 'User logged in');
+        }
+        return back()->withErrors(['email'=>'Invalid credentials'])->onlyInput('email');
+    }
+
+    public function store()
+    {
+        $formfields = request()->validate([
+            'name' => ['required', 'min:2'],
+            'email' => ['required', 'email', 'unique:users'],
+            'password' => ['required', 'confirmed', 'min:6'],
+        ]);
+
+        //  Hash password
+        $formfields['password'] = bcrypt($formfields['password']);
+
+        $user = User::create($formfields);
+
+        auth()->login($user);
+
+        return redirect('/')->with('message', 'User created and logged in succesfully');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/register', [UserController::class, 'create'])->middleware('guest');
+Route::get('/login', [UserController::class, 'login'])->name('login')->middleware('guest');
+Route::post('/users', [UserController::class, 'store']);
+Route::post('/users/authenticate', [UserController::class, 'authenticate']);
